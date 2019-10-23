2264a0912d6b5985b3f6517a254b3bf0c0f69000
Samuel Waithaka <samuel.waithaka@abcthebank.com>
2019-10-23 16:23:00 +0300

    General

--- src/main/java/com/transform/MainApplicationClass.java
+++ src/main/java/com/transform/MainApplicationClass.java
@@ -2,10 +2,18 @@ package com.transform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplicationClass {
+public class MainApplicationClass extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainApplicationClass.class);
+	}
+	
 	public static void main(String[] args) {
-        SpringApplication.run(MainApplicationClass.class, args);
+		SpringApplication.run(MainApplicationClass.class, args);
 	}
 }
